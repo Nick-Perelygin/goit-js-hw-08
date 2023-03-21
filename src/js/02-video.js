3d55e1c786ec775fe28790475426cb3a87451057
Nick-Perelygin <77kalian77@gmail.com>
2023-03-21 19:09:41 +0200

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,13 +7,13 @@ player.on('play', function() {
     console.log('played the video!');
 });
 
-const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
 
 const getCurrentTime = function (currentTime) {
     const seconds = currentTime.seconds;
-    localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds));
+    localStorage.setItem('CURRENT_TIME_KEY', JSON.stringify(seconds));
 };
   
 player.on('timeupdate', getCurrentTime);
   
-player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
+player.setCurrentTime(JSON.parse(localStorage.getItem('CURRENT_TIME_KEY')) || 0);
